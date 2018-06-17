b840db77f99e75ff332ae5067e2126473534510a
BrianDuemmer <bduemmer1@gmail.com>
2018-06-17 10:51:03 -0400

    Moved application.properties to resources package

--- src/com/dystify/kkdystrack3/core/server/Application.java
+++ src/com/dystify/kkdystrack3/core/server/Application.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@PropertySource("resource/application.properties")
 @EntityScan("com.dystify.kkdystrack3.dao.entity")
 @EnableJpaRepositories("com.dystify.kkdystrack3.dao.repository")
 @Import(UiModuleConfiguration.class)
