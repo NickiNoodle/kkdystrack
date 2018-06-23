bba658970d6d579182aae50015dcd41d9153dd3e
BrianDuemmer <bduemmer1@gmail.com>
2018-06-22 23:10:54 -0400

    Got maven script to compile seperate JARs for client / server
    However, they don't run because Spring Boot needs extra setup from a sepatate plugin

---
+++ src/com/dystify/kkdystrack3/common/Foo.java
@@ -0,0 +1,10 @@
+package com.dystify.kkdystrack3.common;
+/**
+ * test class for maven assembly plugin filesets. Need to populate the common package
+ * @author Duemmer
+ *
+ */
+public class Foo
+{
+  int x;
+}
