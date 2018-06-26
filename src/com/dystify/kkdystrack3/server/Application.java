28db31390da89d715ba56fa8932795d3f489ef6a
BrianDuemmer <bduemmer1@gmail.com>
2018-06-26 19:59:39 -0400

    Added debug support to the launcher

--- src/com/dystify/kkdystrack3/server/Application.java
+++ src/com/dystify/kkdystrack3/server/Application.java
@@ -18,6 +18,7 @@ import com.dystify.kkdystrack3.server.module.ui.UiModuleConfiguration;
 public class Application {
 
   public static void main(String[] args) {
+    System.out.println("Hello debug!");
     SpringApplication.run(Application.class, args);
   }
 
